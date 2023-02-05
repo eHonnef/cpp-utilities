5c1ead8c942ee0244778e97e5345a2f0143327fd
honnef <contact@honnef.dev>
2023-02-05 21:57:33 +0100

    Bit/byte operations improvements.

--- byte-utilities/ByteUtilities.hpp
+++ byte-utilities/ByteUtilities.hpp
@@ -36,99 +36,171 @@
 #include <type_traits>
 
 /**
- * @brief Namespace containing the static utilitary functions for bytes/bits operations.
+ * @brief Static class containing the static utilitary functions for bytes/bits operations.
  * All functions does not throw exceptions.
  *
  */
-namespace ByteUtilities {
+class ByteUtilities {
+private:
+  ByteUtilities() = delete;
+  ByteUtilities(const ByteUtilities &) = delete;
+  ByteUtilities &operator=(const ByteUtilities &) = delete;
 
-/**
- * @brief Create a Bit Mask integer. Does not throw exception.
- * Usage Example: aInt & CreateBitMask(0, 10). // Will mask the first 10 bits of aInt
- *
- * @tparam T Type of the bitmask, should be any integer
- * @param nPos Position of the first bit to make the mask.
- * @param nLen Number of bits to include in the bitmask.
- * @return T The bit mask.
- */
-template <typename T> static inline T CreateBitMask(size_t nPos, size_t nLen) noexcept {
-  if constexpr (!std::is_integral_v<T>)
-    static_assert("T should be an integer");
+  /**
+   * @brief Internal usage. Checks in compile time for range and if the given type is an integer.
+   *
+   */
+  template <size_t _uPos, size_t _uLen, typename T> struct SanityCheck {
+    enum {
+      Value = _uLen > 0 && (_uPos + _uLen - 1) < (sizeof(T) * 8) && std::is_integral<T>::value
+    };
+  };
 
-  return ((1U << nLen) - 1U) << nPos;
-}
+public:
+  /*****************************************************************************************************
+   * Bits operation section
+   *****************************************************************************************************/
 
-/**
- * @brief Create a Bit Mask integer in compile time. Throw a compile time error if _nPos is bigger
- * than T in bits, or if _nLen + _nPos is bigger than T in bits.
- * Does not throw exception.
- * Usage Example: aInt & CreateBitMask<int, 0, 10>(). // Will mask the first 10 bits of aInt
- *
- * @tparam T Type of the bitmask, should be any integer
- * @param nPos Position of the first bit to make the mask.
- * @param nLen Number of bits to include in the bitmask.
- * @return constexpr T The bit mask.
- */
-template <typename T, size_t _nPos, size_t _nLen>
-constexpr static inline T CreateBitMask() noexcept {
-  if constexpr (!std::is_integral_v<T> && (_nPos < (sizeof(T) * 8) - 1) &&
-                ((_nLen + _nPos) <= (sizeof(T) * 8)))
-    static_assert(
-        "T should be an integer, _nPos < sizeof T in bits, _nLen + _nPos <= sizeof T in bits");
+  /**
+   * @brief Create a Bit Mask integer. Does not throw exception.
+   * Usage Example: aInt & CreateBitMask(0, 10). // Will mask the first 10 bits of aInt
+   *
+   * @tparam T Type of the bitmask, should be any integer
+   * @param uPos Position of the first bit to make the mask.
+   * @param uLen Number of bits to include in the bitmask.
+   * @return T The bit mask.
+   */
+  template <typename T> static inline T CreateBitMask(size_t uPos, size_t uLen) noexcept {
+    static_assert(std::is_integral<T>::value, "T should be an integer");
 
-  return ((1U << _nLen) - 1U) << _nPos;
-}
+    return ((T(1u) << uLen) - T(1u)) << uPos;
+  }
 
-/**
- * @brief Masks the bits of @ref nInt. Does not throw exception.
- * Usage Example: MaskBits(aInt, 0, 10). // Will mask the first 10 bits of aInt
- *
- * @see @ref CreateBitMask.
- * @tparam T Type of the integer, should be any integer.
- * @param nInt Integer to mask.
- * @param nPos Position of the first bit to mask.
- * @param nLen Number of bits to mask.
- * @return T The masked integer.
- */
-template <typename T> static inline T MaskBits(T nInt, size_t nPos, size_t nLen) noexcept {
-  if constexpr (!std::is_integral_v<T>)
-    static_assert("T should be an integer");
+  /**
+   * @brief Create a Bit Mask integer in compile time.
+   * Usage Example: aInt & CreateBitMask_<int, 0, 10>::Mask. // Will mask the first 10 bits of aInt
+   *
+   * @tparam _uPos Position of the first bit to make the mask.
+   * @tparam _uLen Number of bits to include in the bitmask.
+   * @tparam T Type of the bitmask, should be any integer.
+   */
+  template <size_t _uPos, size_t _uLen, typename T> struct CreateBitMask_ {
+    enum { Mask = ((T(1u) << _uLen) - T(1u)) << _uPos };
+  };
 
-  return nInt & CreateBitMask<T>(nPos, nLen);
-}
+  /**
+   * @brief Return an integer with the slice of bits of @ref nInt shifted to the right. Does not
+   * throw exception. Usage example: GetBitSlice(0x37AB, 9, 5) will return 0x001B. Which the binary
+   * will look like: 0011 0111 1010 1011 -> 0000 0000 0001 1011
+   *
+   * @tparam T Type of the integer, should be any integer.
+   * @param nInt Integer to slice.
+   * @param uPos Position of the first bit to slice.
+   * @param uLen Slice length.
+   * @return T The integer with the slice in the least significative bits.
+   */
+  template <typename T> static inline T GetBitSlice(T nInt, size_t uPos, size_t uLen) noexcept {
+    static_assert(std::is_integral<T>::value, "T should be an integer");
 
-/**
- * @brief Return an integer with the slice of bits of @ref nInt shifted to the right. Does not throw
- * exception. Usage example: GetBitSlice(0x37AB, 9, 5) will return 0x001B. Which the binary will
- * look like: 0011 0111 1010 1011 -> 0000 0000 0001 1011
- *
- * @tparam T Type of the integer, should be any integer.
- * @param nInt Integer to slice.
- * @param nPos Position of the first bit to slice.
- * @param nLen Slice length.
- * @return T The integer with the slice in the least significative bits.
- */
-template <typename T> static inline T GetBitSlice(T nInt, size_t nPos, size_t nLen) noexcept {
-  if constexpr (!std::is_integral_v<T>)
-    static_assert("T should be an integer");
+    return (nInt & CreateBitMask<T>(uPos, uLen)) >> uPos;
+  }
 
-  return MaskBits(nInt, nPos, nLen) >> nPos;
-}
+  /**
+   * @brief Returns the bit value at @ref uPos. Does not throw exception.
+   *
+   * @tparam T Integer's type.
+   * @param nInt Integer to get the bit value from.
+   * @param uPos Bit position to get the value from.
+   * @return true If the bit value is 1 (one).
+   * @return false If the bit value is 0 (zero).
+   */
+  template <typename T> static inline bool GetBit(T nInt, size_t uPos) noexcept {
+    static_assert(std::is_integral<T>::value, "T should be an integer");
 
-/**
- * @brief Returns the byte at Nth position of the integer @ref nInt. Does not throw exception.
- * Usage example: GetByte(0xAB01CD, 1) // Will return 0x01.
- *
- * @tparam T Type of the integer, should be any integer.
- * @param nInt Integer to get the byte from.
- * @param nByte The byte position.
- * @return uint8_t The byte from @ref nInt.
- */
-template <typename T> static inline uint8_t GetByte(T nInt, size_t nByte) noexcept {
-  if constexpr (!std::is_integral_v<T>)
-    static_assert("T should be an integer");
+    return (nInt >> uPos) & T(1u);
+  }
 
-  return (nInt >> (8 * nByte)) & 0xff;
-}
+  /**
+   * @brief Set the value of a single bit of @ref nInt at @ref uPos using @ref bBitValue. Inplace
+   * operation. Does not throw exception.
+   *
+   * @tparam T Integer's type. Inplace operation, this integer will be changed.
+   * @param nInt Integer to set the bit value. Inplace operation, this integer will be changed.
+   * @param uPos Bit position to set the value
+   * @param bBitValue Bit value to be set, true (or 1) will set the bit to 1 (one), false will set
+   * the bit to 0 (zero).
+   */
+  template <typename T> static inline void SetBit(T &nInt, size_t uPos, bool bBitValue) noexcept {
+    static_assert(std::is_integral<T>::value, "T should be an integer");
 
-}; // namespace ByteUtilities
+    nInt ^= (-(!!T(bBitValue)) ^ nInt) & (T(1u) << uPos);
+  }
+
+  /**
+   * @brief Flips the bit value of @ref nInt at @ref uPos. Inplace operation. Does not throw
+   * exception.
+   *
+   * @tparam T Integer's type.
+   * @param nInt Integer to set the bit value. Inplace operation, this integer will be changed.
+   * @param uPos Bit position to flip.
+   */
+  template <typename T> static inline void FlipBit(T &nInt, size_t uPos) noexcept {
+    static_assert(std::is_integral<T>::value, "T should be an integer");
+
+    nInt ^= T(1u) << uPos;
+  }
+
+  /*****************************************************************************************************
+   * Bytes operation section
+   *****************************************************************************************************/
+
+  /**
+   * @brief Returns the byte at Nth position of the integer @ref nInt. Does not throw exception.
+   * Usage example: GetByte(0xAB01CD, 1) // Will return 0x01.
+   *
+   * @tparam T Type of the integer, should be any integer.
+   * @param nInt Integer to get the byte from.
+   * @param uPos The byte position.
+   * @return uint8_t The byte from @ref nInt.
+   */
+  template <typename T> static inline uint8_t GetByte(T nInt, size_t uPos) noexcept {
+    static_assert(std::is_integral<T>::value, "T should be an integer");
+
+    return (nInt >> (8 * uPos)) & 0xff;
+  }
+
+  /**
+   * @brief Returns the byte at Nth position of the integer @ref nInt. Creates the mask in compile
+   * time, apply the mask in runtime. Does not throw exception.
+   * Usage example: GetByte<1>(0xAB01CD) // Will return 0x01.
+   *
+   * @tparam _uPos The byte position.
+   * @tparam T Type of the integer, should be any integer.
+   * @param nInt Integer to get the byte from.
+   * @return uint8_t The byte from @ref nInt.
+   */
+  template <size_t _uPos, typename T> static inline uint8_t GetByte(T nInt) noexcept {
+    static_assert(SanityCheck<(_uPos * 8), (8 - 1), T>::Value,
+                  "T should be an integer || [_uPos] is out of bounds");
+
+    return (nInt & CreateBitMask_<_uPos * 8, 8, T>::Mask) >> (_uPos * 8);
+  }
+
+  /**
+   * @brief Will set the value of @ref nInt. Inplace operation. Does not throw exception.
+   * Usage example: int nInt = 0xAB01CD; SetBye<1>(nInt, 0xFF); // Will set 0xAB01CD to 0xABFFCD.
+   *
+   * @tparam _uPos Byte position.
+   * @tparam T Integer's type.
+   * @param[out] nInt Integer to set byte, Inplace operation, this integer will be changed.
+   * @param uByteValue Byte value to be written.
+   */
+  template <size_t _uPos, typename T>
+  static inline void SetByte(T &nInt, uint8_t uByteValue) noexcept {
+    static_assert(SanityCheck<(_uPos * 8), (8 - 1), T>::Value,
+                  "T should be an integer || [_uPos] is out of bounds");
+
+    nInt |= (CreateBitMask_<_uPos * 8, 8, T>::Mask & (static_cast<T>(uByteValue) << _uPos * 8));
+  }
+
+}; // class ByteUtilities
--- byte-utilities/Tests.cpp
+++ byte-utilities/Tests.cpp
@@ -6,57 +6,138 @@
 
 #include <ByteUtilities.hpp>
 #include <catch_amalgamated.hpp>
+#include <typeinfo>
 
 /**************************************************************************************
  * Test Section for [Bit operation]
  **************************************************************************************/
 
-TEMPLATE_TEST_CASE("Bit mask test", "[Bit operation]", int, long long int, size_t, uint16_t,
-                   uint32_t, uint64_t) {
+TEMPLATE_TEST_CASE("Bit mask test - compile time", "[Bit operation]", int16_t, int32_t, int64_t,
+                   size_t, uint16_t, uint32_t, uint64_t) {
   constexpr TestType nValueA = 0x37AB;
   constexpr TestType nValueB = 0xB7AB;
 
   // 0011 0111 1010 1011 -> 0011 0110 0000 0000
-  REQUIRE(ByteUtilities::MaskBits(nValueA, 9, 5) == 0x3600);
+  REQUIRE((ByteUtilities::CreateBitMask_<9, 5, TestType>::Mask & nValueA) == 0x3600);
 
   // 0011 0111 1010 1011 -> 0000 0000 0000 0001
-  REQUIRE(ByteUtilities::MaskBits(nValueA, 0, 1) == 0x0001);
+  REQUIRE((ByteUtilities::CreateBitMask_<0, 1, TestType>::Mask & nValueA) == 0x0001);
 
   // 0011 0111 1010 1011 -> 0000 0000 0000 0000
-  REQUIRE(ByteUtilities::MaskBits(nValueA, 0, 0) == 0x0000);
+  REQUIRE((ByteUtilities::CreateBitMask_<0, 0, TestType>::Mask & nValueA) == 0x0000);
 
   // 1011 0111 1010 1011 -> 1000 0000 0000 0000
-  REQUIRE(ByteUtilities::MaskBits(nValueB, 15, 1) == 0x8000);
-
-  // 1011 0111 1010 1011 -> 0000 0000 0000 0000
-  REQUIRE(ByteUtilities::MaskBits(nValueB, 15, 0) == 0x0000);
-}
-
-TEMPLATE_TEST_CASE("Bit mask test - compile time", "[Bit operation]", int, long long int, size_t,
-                   uint16_t, uint32_t, uint64_t) {
-  constexpr TestType nValueA = 0x37AB;
-  constexpr TestType nValueB = 0xB7AB;
-
-  // 0011 0111 1010 1011 -> 0011 0110 0000 0000
-  REQUIRE((ByteUtilities::CreateBitMask<TestType, 9, 5>() & nValueA) == 0x3600U);
-
-  // 0011 0111 1010 1011 -> 0000 0000 0000 0001
-  REQUIRE((ByteUtilities::CreateBitMask<TestType, 0, 1>() & nValueA) == 0x0001U);
+  REQUIRE((ByteUtilities::CreateBitMask_<15, 1, TestType>::Mask & nValueB) == 0x8000);
 
   // 0011 0111 1010 1011 -> 0000 0000 0000 0000
-  REQUIRE((ByteUtilities::CreateBitMask<TestType, 0, 0>() & nValueA) == 0x0000U);
-
-  // 1011 0111 1010 1011 -> 1000 0000 0000 0000
-  REQUIRE((ByteUtilities::CreateBitMask<TestType, 15, 1>() & nValueB) == 0x8000U);
-
-  // 0011 0111 1010 1011 -> 0000 0000 0000 0000
-  REQUIRE((ByteUtilities::CreateBitMask<TestType, 15, 0>() & nValueB) == 0x0000U);
+  REQUIRE((ByteUtilities::CreateBitMask_<15, 0, TestType>::Mask & nValueB) == 0x0000);
 }
 
 TEST_CASE("Bit slice test", "[Bit operation]") {
   // 0011 0111 1010 1011 -> 0000 0000 0001 1011
   REQUIRE(ByteUtilities::GetBitSlice(0x37AB, 9, 5) == 0x001B);
   // @todo: more test cases
+}
+
+TEMPLATE_TEST_CASE("Set bit", "[Bit operation]", int16_t, int32_t, int64_t, size_t, uint16_t,
+                   uint32_t, uint64_t) {
+  // 1011 0111 1010 1011
+  TestType nValue = 0xB7AB;
+
+  ByteUtilities::SetBit(nValue, 15, false);
+  REQUIRE(nValue == TestType(0x37ab));
+
+  ByteUtilities::SetBit(nValue, 15, 1);
+  REQUIRE(nValue == TestType(0xB7ab));
+
+  ByteUtilities::SetBit(nValue, 0, false);
+  REQUIRE(nValue == TestType(0xB7aa));
+
+  ByteUtilities::SetBit(nValue, 0, true);
+  REQUIRE(nValue == TestType(0xB7ab));
+
+  ByteUtilities::SetBit(nValue, 6, true);
+  REQUIRE(nValue == TestType(0xB7eb));
+
+  ByteUtilities::SetBit(nValue, 6, 0);
+  REQUIRE(nValue == TestType(0xB7ab));
+
+  // Set most significant bit
+  nValue = 0;
+
+  ByteUtilities::SetBit(nValue, (sizeof(TestType) * 8) - 1, 1);
+
+  if constexpr (sizeof(TestType) == 2)
+    REQUIRE(nValue == TestType(0x80'00));
+  else if constexpr (sizeof(TestType) == 4)
+    REQUIRE(nValue == TestType(0x80'00'00'00));
+  else if constexpr (sizeof(TestType) == 8)
+    REQUIRE(nValue == TestType(0x80'00'00'00'00'00'00'00));
+  else
+    static_assert(std::is_integral<TestType>::value && false, "Missing test case");
+}
+
+TEMPLATE_TEST_CASE("Get bit", "[Bit operation]", int16_t, int32_t, int64_t, size_t, uint16_t,
+                   uint32_t, uint64_t) {
+  // 1011 0111 1010 1011
+  TestType nValue = 0xB7AB;
+
+  REQUIRE(ByteUtilities::GetBit(nValue, 0) == 1);
+  REQUIRE(ByteUtilities::GetBit(nValue, 2) == 0);
+  REQUIRE(ByteUtilities::GetBit(nValue, 15) == true);
+  REQUIRE(ByteUtilities::GetBit(nValue, 11) == false);
+
+  if constexpr (sizeof(TestType) == 2) {
+    REQUIRE(ByteUtilities::GetBit(TestType(0x8000), 0) == false);
+    REQUIRE(ByteUtilities::GetBit(TestType(0x8000), (sizeof(TestType) * 8) - 1) == 1);
+  } else if constexpr (sizeof(TestType) == 4) {
+    REQUIRE(ByteUtilities::GetBit(TestType(0x80'00'00'00), 0) == false);
+    REQUIRE(ByteUtilities::GetBit(TestType(0x80'00'00'00), (sizeof(TestType) * 8) - 1) == 1);
+  } else if constexpr (sizeof(TestType) == 8) {
+    REQUIRE(ByteUtilities::GetBit(TestType(0x80'00'00'00'00'00'00'00), 0) == false);
+    REQUIRE(ByteUtilities::GetBit(TestType(0x80'00'00'00'00'00'00'00),
+                                  (sizeof(TestType) * 8) - 1) == 1);
+  } else
+    static_assert(std::is_integral<TestType>::value && false, "Missing test case");
+}
+
+TEMPLATE_TEST_CASE("Flip bit", "[Bit operation]", int16_t, int32_t, int64_t, size_t, uint16_t,
+                   uint32_t, uint64_t) {
+  // 1011 0111 1010 1011
+  TestType nValue = 0xB7AB;
+
+  ByteUtilities::FlipBit(nValue, 15);
+  REQUIRE(nValue == TestType(0x37ab));
+
+  ByteUtilities::FlipBit(nValue, 15);
+  REQUIRE(nValue == TestType(0xB7ab));
+
+  ByteUtilities::FlipBit(nValue, 0);
+  REQUIRE(nValue == TestType(0xB7aa));
+
+  ByteUtilities::FlipBit(nValue, 0);
+  REQUIRE(nValue == TestType(0xB7ab));
+
+  ByteUtilities::FlipBit(nValue, 6);
+  REQUIRE(nValue == TestType(0xB7eb));
+
+  ByteUtilities::FlipBit(nValue, 6);
+  REQUIRE(nValue == TestType(0xB7ab));
+
+  // Flip most and least significant bit
+  nValue = TestType(0x1);
+  ByteUtilities::FlipBit(nValue, 0); // turn to 0x0
+  ByteUtilities::FlipBit(nValue, (sizeof(TestType) * 8) - 1);
+  ByteUtilities::FlipBit(nValue, 3);
+
+  if constexpr (sizeof(TestType) == 2)
+    REQUIRE(nValue == TestType(0x80'08));
+  else if constexpr (sizeof(TestType) == 4)
+    REQUIRE(nValue == TestType(0x80'00'00'08));
+  else if constexpr (sizeof(TestType) == 8)
+    REQUIRE(nValue == TestType(0x80'00'00'00'00'00'00'08));
+  else
+    static_assert(std::is_integral<TestType>::value && false, "Missing test case");
 }
 
 /**************************************************************************************
@@ -75,4 +156,44 @@ TEST_CASE("Get byte test", "[Byte operation]") {
   REQUIRE(ByteUtilities::GetByte(nInt, 5) == 0xEFu);
   REQUIRE(ByteUtilities::GetByte(nInt, 6) == 0xCDu);
   REQUIRE(ByteUtilities::GetByte(nInt, 7) == 0xABu);
+}
+
+TEST_CASE("Get byte test - compile time", "[Byte operation]") {
+  // 0xAB CD EF 01 23 45 67 89
+  volatile uint64_t nInt = 0xABCDEF0123456789;
+
+  volatile uint64_t *pInt = new uint64_t();
+  *pInt = nInt;
+
+  REQUIRE(ByteUtilities::GetByte<0>(*pInt) == 0x89u);
+  REQUIRE(ByteUtilities::GetByte<1>(*pInt) == 0x67u);
+  REQUIRE(ByteUtilities::GetByte<2>(*pInt) == 0x45u);
+  REQUIRE(ByteUtilities::GetByte<3>(*pInt) == 0x23u);
+  REQUIRE(ByteUtilities::GetByte<4>(*pInt) == 0x01u);
+  REQUIRE(ByteUtilities::GetByte<5>(*pInt) == 0xEFu);
+  REQUIRE(ByteUtilities::GetByte<6>(*pInt) == 0xCDu);
+  REQUIRE(ByteUtilities::GetByte<7>(*pInt) == 0xABu);
+
+  delete pInt;
+}
+
+TEST_CASE("Set byte test - compile time", "[Byte operation]") {
+  // 0xAB CD EF 01 23 45 67 89
+  constexpr uint64_t uResult = 0xABCDEF0123456789;
+
+  volatile uint64_t *pInt = new uint64_t();
+  *pInt = 0;
+
+  ByteUtilities::SetByte<0>(*pInt, 0x89);
+  ByteUtilities::SetByte<1>(*pInt, 0x67);
+  ByteUtilities::SetByte<2>(*pInt, 0x45);
+  ByteUtilities::SetByte<3>(*pInt, 0x23);
+  ByteUtilities::SetByte<4>(*pInt, 0x01);
+  ByteUtilities::SetByte<5>(*pInt, 0xEF);
+  ByteUtilities::SetByte<6>(*pInt, 0xCD);
+  ByteUtilities::SetByte<7>(*pInt, 0xAB);
+
+  REQUIRE(*pInt == uResult);
+
+  delete pInt;
 }
